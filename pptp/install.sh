08c1b84f49d42aeaec0db015afb95c75b73b88f8
bedefaced <thisisnewlife@mail.ru>
2017-04-14 17:49:49 +0300

    quotes fix

--- pptp/install.sh
+++ pptp/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source $DIR/env.sh
 
 if [[ "$EUID" -ne 0 ]]; then

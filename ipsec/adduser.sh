459db4b62a16ed617b9c762a492d82db59ad84cc
bedefaced <thisisnewlife@mail.ru>
2017-08-30 01:57:18 +0300

    openvpn check and deluser (revoke); crl-verify option added; adduser bugfix

--- ipsec/adduser.sh
+++ ipsec/adduser.sh
@@ -48,6 +48,7 @@ do
 			else
 				read -p "Would you want to add another user? [no] " ANSUSER
 				: ${ANSUSER:=$NOTADDUSER}
+				unset LOGIN
 			fi
 			continue
 		else
@@ -101,6 +102,7 @@ do
 		echo
 		read -p "Would you want to add another user? [no] " ANSUSER
 		: ${ANSUSER:=$NOTADDUSER}
+		unset LOGIN
 	else
 		ANSUSER=$NOTADDUSER
 	fi
